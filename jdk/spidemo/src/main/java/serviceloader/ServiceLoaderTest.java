7727a5bf29a783afdf4cdeeb593b2f39adf936e2
孙显顺 <sunxs@tuniondata.com>
2017-07-19 11:40:39 +0800

    add java spi demo2

--- jdk/spidemo/src/main/java/serviceloader/ServiceLoaderTest.java
+++ jdk/spidemo/src/main/java/serviceloader/ServiceLoaderTest.java
@@ -13,14 +13,19 @@ public class ServiceLoaderTest {
     public static void main(String[] args) {
 
         ServiceLoader<MyInterface> serviceLoader = ServiceLoader.load(MyInterface.class);
-        Iterator<MyInterface> impls = serviceLoader.iterator();
+//        Iterator<MyInterface> impls = serviceLoader.iterator();
+//        while (impls.hasNext()) {
+//            MyInterface myInterface = impls.next();
+//
+//            String ret = myInterface.sayHello("sxs");
+//            System.out.println(ret);
+//        }
 
-        while (impls.hasNext()) {
-            MyInterface myInterface = impls.next();
-
-            String ret = myInterface.sayHello("sxs");
+        for(MyInterface myInterface : serviceLoader){
+            String ret = myInterface.sayHello("qwe");
             System.out.println(ret);
         }
+
 
 
     }
