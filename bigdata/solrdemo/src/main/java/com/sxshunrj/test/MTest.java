8f42f20a616dc57b918b30b43e1349fcf4100040
孙显顺 <sunxs@tuniondata.com>
2017-07-21 11:56:50 +0800

    add solr demo

--- bigdata/solrdemo/src/main/java/com/sxshunrj/test/MTest.java
+++ bigdata/solrdemo/src/main/java/com/sxshunrj/test/MTest.java
@@ -34,7 +34,7 @@ public class MTest {
         SolrInputDocument doc = new SolrInputDocument();
         doc.addField("id", "id");
         // 其他参数省略。。。
-        doc.addField("title", "我深深地爱着你，solr");
+        doc.addField("zhname", "冒充韩国艺人骗钱");
         sc.add(doc);
         sc.commit();
     }
@@ -93,14 +93,14 @@ public class MTest {
         //遍历结果集
         for (SolrDocument solrDocument : solrDocumentList) {
             System.out.println(solrDocument); //打印为：SolrDocument{id=id, content_test=[我深深地爱着你，solr !], _version_=1560544234369449984}
-            System.out.println(solrDocument.get("id"));
-            System.out.println(solrDocument.get("title"));
-            System.out.println(solrDocument.get("_version_"));
+//            System.out.println(solrDocument.get("id"));
+//            System.out.println(solrDocument.get("title"));
+//            System.out.println(solrDocument.get("_version_"));
         }
     }
 
     //条件查询
-    private static void selectDoc() throws SolrServerException, IOException{
+    private static void selectDoc(String key, String val) throws SolrServerException, IOException{
         //得到查询连接
         SolrClient sc = getSolrClient();
         SolrQuery query = new SolrQuery();
@@ -109,34 +109,34 @@ public class MTest {
 
         /*1、常用
         q :查询字符串，这个是必须的。如果查询所有*:* ，根据指定字段查询（Name:张三 AND Address:北京）  注意：AND要大写 否则会被当做默认OR*/
-        query.setQuery("*:*");//id:id AND title:solr
+        query.setQuery(key+":"+val);//id:id AND title:solr
 
         /*fq :
          * （filter query）过虑查询，作用：在q查询符合结果中同时是fq查询符合的，
          * 例如：q=查询全部&fq=只要title得值为:伟帅你是我偶吧*/
-        query.setFilterQueries("title:伟帅你是我偶吧");
+//        query.setFilterQueries("title:伟帅你是我偶吧");
 
         /*fl :
          *  指定返回那些字段内容，用逗号或空格分隔多个。 */
-        query.setFields("id,title"); //只返回id 和title
+//        query.setFields("id,title"); //只返回id 和title
 
         /*start :
          * 返回第一条记录在完整找到结果中的偏移位置，0开始，一般分页用。*/
-        query.setStart(0);
+//        query.setStart(0);
 
         /*rows :
          *  指定返回结果最多有多少条记录，配合start来实现分页。*/
-        query.setRows(4);
+//        query.setRows(4);
 
         /*sort :
          *  排序，格式：sort=<field name>+<desc|asc>[,<field name>+<desc|asc>]… 。
          *  示例：（score desc, price asc）表示先 “score” 降序, 再 “price” 升序，默认是相关性降序。 */
-        SolrQuery.SortClause sort=new SolrQuery.SortClause("id", SolrQuery.ORDER.desc);
-        query.setSort(sort);//可以添加集合
+//        SolrQuery.SortClause sort=new SolrQuery.SortClause("id", SolrQuery.ORDER.desc);
+//        query.setSort(sort);//可以添加集合
 
         /*hl
          *  是否高亮 ,如hl=true*/
-        query.setHighlight(true);
+//        query.setHighlight(true);
         /*【注：以上是比较常用的参数，当然具体的参数使用还是多看Solr官方的技术文档以及一些大神的博文日志，这里只是抛砖引玉】*/
         /*二、 Solr运算符
         1. “:” 指定字段查指定值，如返回所有值*:*
@@ -154,13 +154,9 @@ public class MTest {
         13. {} 不包含范围检索，如检索某时间段记录，不包含头尾
         date:{200707 TO 200710}
         14. / 转义操作符，特殊字符包括+ - && || ! ( ) { } [ ] ^ ” ~ * ? : /
-
          注：①“+”和”-“表示对单个查询单元的修饰，and 、or 、 not 是对两个查询单元是否做交集或者做差集还是取反的操作的符号
-
         　　 比如:AB:china +AB:america ,表示的是AB:china忽略不计可有可无，必须满足第二个条件才是对的,而不是你所认为的必须满足这两个搜索条件
-
         　　 如果输入:AB:china AND AB:america ,解析出来的结果是两个条件同时满足，即+AB:china AND +AB:america或+AB:china +AB:america
-
         　　总而言之，查询语法：  修饰符 字段名:查询关键词 AND/OR/NOT 修饰符 字段名:查询关键词
 */
         //查询
@@ -168,9 +164,9 @@ public class MTest {
         //遍历结果集
         for (SolrDocument solrDocument : solrDocumentList) {
             System.out.println(solrDocument); //打印为：SolrDocument{id=id, content_test=[我深深地爱着你，solr !], _version_=1560544234369449984}
-            System.out.println(solrDocument.get("id"));
-            System.out.println(solrDocument.get("title"));
-            System.out.println(solrDocument.get("_version_"));
+//            System.out.println(solrDocument.get("id"));
+//            System.out.println(solrDocument.get("title"));
+//            System.out.println(solrDocument.get("_version_"));
         }
     }
 
@@ -187,7 +183,8 @@ public class MTest {
         // 根据Id查询一条数据
 //         getDocById();
         // 全部查询
-        getDocByAll();
+//        getDocByAll();
+        selectDoc("zhname","骗人");
     }
 
     /**
