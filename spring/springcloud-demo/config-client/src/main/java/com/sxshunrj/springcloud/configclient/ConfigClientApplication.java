72fc71b82b06c89035fbb806c0925b80a8fdfc0e
孙显顺 <sxshunrj@163.com>
2018-01-23 18:08:45 +0800

    消息总线(Spring Cloud Bus)

--- spring/springcloud-demo/config-client/src/main/java/com/sxshunrj/springcloud/configclient/ConfigClientApplication.java
+++ spring/springcloud-demo/config-client/src/main/java/com/sxshunrj/springcloud/configclient/ConfigClientApplication.java
@@ -1,12 +1,16 @@
 package com.sxshunrj.springcloud.configclient;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * POST http://127.0.0.1:19908/bus/refresh
+ */
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
